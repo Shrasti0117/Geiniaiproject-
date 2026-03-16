b2e0ecc22ecb8e6aa0bbde77371e35d3210f5667
shrasti gupta <guptashrasti25@gmail.com>
2026-03-16 18:55:16 +0530

    app.jsx changed commit

--- gemini/src/App.jsx
+++ gemini/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Main from './components/Main/main'
+import Main from './components/Main/Main'
 import Sidebar from './components/sidebar/Sidebar'
 
 const App = () => {
